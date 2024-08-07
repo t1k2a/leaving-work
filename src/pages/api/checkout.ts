eb40685c124224f78f06965717f45dbfbd160060
t1k2a <maxtj1122@gmail.com>
2024-08-07 22:44:26 +0900

    ngrokを環境変数にする #17

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -36,7 +36,7 @@ export default async (req: any, res: any) => {
     );
 
     await axios.post(
-      "https://aebc-2404-7a81-2061-a00-8ff-df18-5809-6830.ngrok-free.app/api",
+      `${process.env.NGROK_URL}`,
       { text: `${text}` },
       {
         headers: {
